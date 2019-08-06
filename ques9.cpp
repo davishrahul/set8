8b38459aa6343670aa31bdfe12ab10780eeab353
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 21:31:13 +0530

    Create ques9.cpp

---
+++ ques9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number1,count=0,number2;
+    cin>>number1>>number2;
+    int product=number1*number2;
+    for(int i=1;i<=product;i++)
+    {
+        if(product==i*i)
+        {
+            count++;
+        }
+    }
+    if(count>0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }   
+}
