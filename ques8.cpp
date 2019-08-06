8bdd5a569db0bcee095187f634f828f31a8faac2
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 21:17:30 +0530

    Create ques8.cpp

---
+++ ques8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number%13==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    
+}
